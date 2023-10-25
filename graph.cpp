7f3f715ecaf1c683f119108ff43cf0fcfe4adc80
Ayush Sharma <ashsharman123@gmail.com>
2023-10-25 17:18:33 +0530

    adjacency list

--- graph.cpp
+++ graph.cpp
@@ -1,7 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+vector<vector<int>> printAdjacency(int n, int m, vector<vector<int>> &edges)
+{
+
+    vector<int> graph[n];
+
+    for (int i = 0; i < m; i++)
+    {
+        int u = edges[i][0];
+        int v = edges[i][1];
+
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+
+    vector<vector<int>> adj(n);
+    for (int i = 0; i < n; i++)
+    {
+        adj[i].push_back(i);
+        for (int j = 0; j < graph[i].size(); j++)
+        {
+            adj[i].push_back(graph[i][j]);
+        }
+    }
+    return adj;
+}
+
+int main()
+{
     int graph[4][4] = {0}; // Initialize the adjacency matrix with all zeros
 
     // Create the directed edges
@@ -11,8 +38,10 @@ int main() {
     graph[0][3] = 1; // d to a
 
     // Output the adjacency matrix
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
             cout << graph[i][j] << " ";
         }
         cout << endl;
