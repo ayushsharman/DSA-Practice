e95671cef35a6a6d3c8925c7bfd6c980d83facca
Ayush Sharma <ashsharman123@gmail.com>
2024-01-19 10:19:55 +0530

    another approach

--- bfs_walls_gates.cpp
+++ bfs_walls_gates.cpp
@@ -49,3 +49,44 @@ private:
         return i >= 0 && i < m && j >= 0 && j < n;
     }
 };
+
+
+
+
+/* class Solution {
+public:
+    void islandsAndTreasure(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        queue<pair<int, int>> q;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 0) {
+                    q.push({i, j});
+                }
+            }
+        }
+        
+        while (!q.empty()) {
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+            
+            // Classic BFS: iterate through adjacent cells
+            int dx[] = {-1, 1, 0, 0};
+            int dy[] = {0, 0, -1, 1};
+            
+            for (int i = 0; i < 4; i++) {
+                int x = row + dx[i];
+                int y = col + dy[i];
+                
+                if (x >= 0 && x < m && y >= 0 && y < n && grid[x][y] != INT_MAX) {
+                    grid[x][y] = grid[row][col] + 1;
+                    q.push({x, y});
+                }
+            }
+        }
+    }
+};
+ */
